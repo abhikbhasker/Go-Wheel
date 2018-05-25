8ec70c3c46ef7f962ac8b1ec021de2484829edec
Abhishek Bhaskar <abhikbhasker2@gmail.com>
2018-05-25 19:49:03 +0530

    Add files via upload

---
+++ code/gyro/gyro.ino
@@ -0,0 +1,21 @@
+
+#include <SoftwareSerial.h>
+SoftwareSerial BtSerial(4,5); // RX | TX
+int old=0;
+float i=0;
+void setup() {
+  Serial.begin(9600); // Default communication rate of the Bluetooth module
+  BtSerial.begin(9600);
+}
+void loop() {
+ int throttle = 150; // Reads the data from the serial port4
+    if(Serial.available()>0){
+      int xx=Serial.read();
+      for(i=0;i<45;i+=.01){
+        BtSerial.write(xx);
+      }
+    }
+    
+    
+ 
+}
---
+++ code/wheel/wheel.ino
@@ -0,0 +1,22 @@
+#define throttlePin 3
+
+#include <SoftwareSerial.h>
+SoftwareSerial BtSerial(4,5); // RX | TX
+void setup() {
+  pinMode(3,OUTPUT);
+  Serial.begin(9600);
+  BtSerial.begin(9600);// Default communication rate of the Bluetooth module
+}
+void loop() {
+ if(BtSerial.available() > 0){ // Checks whether data is comming from the serial port
+    int throttle = BtSerial.read(); // Reads the data from the serial port
+    throttle=throttle*13;
+    if(throttle>=255){
+    throttle=255;
+      
+    }
+    Serial.println(throttle);
+    analogWrite(throttlePin, throttle);
+ }
+ 
+}
